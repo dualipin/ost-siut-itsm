7fcb5c5d5381630237b9532d5a35b7f59306d785
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-09-26 23:02:25 -0600

    fix: actualizar el encabezado de prohibido

--- prohibido.php
+++ prohibido.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types=1);
 
-header('HTTP/1.1 404 Not Found');
+header('HTTP/1.1 403 Forbidden');
 
-/* @var Latte\Engine $latte */
-$latte = require_once __DIR__ . '/src/latte.php';
+require_once __DIR__ . '/src/configuracion.php';
 
-$templatePath = __DIR__ . '/plantillas/no-encontrado.latte';
+$latte = \App\Fabricas\FabricaLatte::obtenerInstancia();
+
+$templatePath = __DIR__ . '/plantillas/prohibido.latte';
 
 $latte->render($templatePath);
