2d5bf825f14fc24550ac094093e79b685750c3b5
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-09-27 00:01:53 -0600

    fix: corregir pagina no encontrada

---
+++ aplicacion/no-encontrado.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use App\Fabricas\FabricaLatte;
+
+require_once __DIR__ . '/../src/configuracion.php';
+
+FabricaLatte::obtenerInstancia()
+        ->render(__DIR__ . '/plantillas/no-encontrado.latte');
