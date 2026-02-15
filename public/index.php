83cbd8f4ab16826b2f6464ddd7364fbe41daee66
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-02-15 15:56:43 -0600

    feat: add index.php to initialize application and render index.latte

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Bootstrap;
+use App\Infrastructure\Env\EnvironmentInterface;
+use App\Infrastructure\Templating\RendererInterface;
+
+require __DIR__ . "/../bootstrap.php";
+
+$container = Bootstrap::buildContainer();
+
+$env = $container->get(EnvironmentInterface::class);
+
+$renderer = $container->get(RendererInterface::class);
+
+$renderer->render("./index.latte");
