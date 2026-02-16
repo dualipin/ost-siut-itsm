59ce7188d451bfafe136fae650a1f8437ec23ce1
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-02-16 17:57:45 -0600

    feat: update sidebar animations and navigation links; add rendering for activos and solicitar pages

--- public/portal/usuario/listado.php
+++ public/portal/usuario/listado.php
@@ -13,32 +13,32 @@ $renderer = $container->get(RendererInterface::class);
 $usuarios = $container->get(UsuarioRepository::class);
 
 $renderer->render("./listado.latte", [
-    //    "usuarios" => $usuarios->listado(),
-    "usuarios" => [
-        new UsuarioSimpleDTO(
-            id: "1",
-            nombre: "Juan",
-            apellidos: "Pérez",
-            email: "correo@correo.com",
-            rol: \App\Module\Usuario\Entity\RolEnum::Admin,
-            activo: true,
-            departamento: "dasdasd",
-        ),
-        new UsuarioSimpleDTO(
-            id: "2",
-            nombre: "Juan adasd",
-            apellidos: "Pérez",
-            email: "correo@correo.com",
-            rol: \App\Module\Usuario\Entity\RolEnum::Finanzas,
-            activo: true,
-        ),
-        new UsuarioSimpleDTO(
-            id: "3",
-            nombre: "Juandasdd",
-            apellidos: "Pérez",
-            email: "correo@correo.com",
-            rol: \App\Module\Usuario\Entity\RolEnum::Agremiado,
-            activo: true,
-        ),
-    ],
+    "usuarios" => $usuarios->listado(),
+    // "usuarios" => [
+    //     new UsuarioSimpleDTO(
+    //         id: "1",
+    //         nombre: "Juan",
+    //         apellidos: "Pérez",
+    //         email: "correo@correo.com",
+    //         rol: \App\Module\Usuario\Entity\RolEnum::Admin,
+    //         activo: true,
+    //         departamento: "dasdasd",
+    //     ),
+    //     new UsuarioSimpleDTO(
+    //         id: "2",
+    //         nombre: "Juan adasd",
+    //         apellidos: "Pérez",
+    //         email: "correo@correo.com",
+    //         rol: \App\Module\Usuario\Entity\RolEnum::Finanzas,
+    //         activo: true,
+    //     ),
+    //     new UsuarioSimpleDTO(
+    //         id: "3",
+    //         nombre: "Juandasdd",
+    //         apellidos: "Pérez",
+    //         email: "correo@correo.com",
+    //         rol: \App\Module\Usuario\Entity\RolEnum::Agremiado,
+    //         activo: true,
+    //     ),
+    // ],
 ]);
