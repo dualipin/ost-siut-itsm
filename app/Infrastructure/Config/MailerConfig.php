ade65bc128e5b29538223707de3b8ca39e29e70f
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-03-04 20:33:54 -0600

    fix: Add missing parameters from MailerConfig constructor

--- app/Infrastructure/Config/MailerConfig.php
+++ app/Infrastructure/Config/MailerConfig.php
@@ -9,7 +9,8 @@ final readonly class MailerConfig
         public string $user,
         public string $password,
         public int $port,
+        public string $fromAddress,
+        public string $fromName,
         public string $charset,
-    ) {
-    }
+    ) {}
 }
