5ef1a3d3f232607735fece9702d41eb5f9506c9f
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-03-04 22:16:03 -0600

    refactor: Remove unused CalculadoraInteresSimpleTest file

--- tests/Unit/Module/Prestamo/Util/CalculadoraInteresSimpleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Module\Prestamo\Util\CalculadoraInteresSimple;
-
-it("verificar calculo de interés simple", function () {
-    CalculadoraInteresSimple::calcular(1000, 0.04, 6);
-});
