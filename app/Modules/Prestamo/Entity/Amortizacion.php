d3f63fbde697adfdad0bbe46789c58038c1d9413
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-03-06 09:16:54 -0600

    Remove unused entities, enums, repositories, and services from the Prestamo, Publicacion, Sindicato, and Usuario modules to streamline the codebase and improve maintainability.

--- app/Modules/Prestamo/Entity/Amortizacion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Module\Prestamo\Entity;
-
-final readonly class Amortizacion
-{
-    public function __construct(public ?int $id = null) {}
-}
--- app/Modules/Prestamo/Entity/Documento.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Module\Prestamo\Entity;
-
-final readonly class Documento
-{
-    public function __construct(public ?int $id = null) {}
-}
