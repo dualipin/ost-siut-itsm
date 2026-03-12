9dd6e6a9c9e870f9ad8cdab2cac3fbdfee56d7a4
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-03-11 18:55:04 -0600

    feat: Implement PublicationRepositoryInterface methods in PdoPublicationRepository

--- app/Modules/Publication/Infrastructure/Persistence/PdoPublicationRepository.php
+++ app/Modules/Publication/Infrastructure/Persistence/PdoPublicationRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Modules\Publication\Infrastructure\Persistence;
 
+use App\Modules\Publication\Domain\Entity\Publication;
+use App\Modules\Publication\Domain\Enum\PublicationTypeEnum;
 use App\Modules\Publication\Domain\Repository\PublicationRepositoryInterface;
 
 final readonly class PdoPublicationRepository implements
-    PublicationRepositoryInterface {}
+    PublicationRepositoryInterface
+{
+    public function findByType(PublicationTypeEnum $type): array
+    {
+        $sql =
+            "SELECT publication_id FROM publications where publication_type = :type";
+    }
+
+    public function findById(int $id): ?Publication
+    {
+        // TODO: Implement findById() method.
+    }
+}
