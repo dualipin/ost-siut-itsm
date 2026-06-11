928ed1efd2847c42a29972353cfeac6fdefacc6d
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-06-10 21:25:09 -0600

    feat: remove unused components and utility functions from the project

--- resources/js/apps/common/utils/date.ts
+++
@@ -1,6 +0,0 @@
-export function localize(dateStr: string){
-        if (!dateStr) return ''
-        const p = String(dateStr).split('-')
-        if (p.length !== 3) return dateStr
-        return `${ p[2] }/${ p[1] }/${ p[0] }`
-}
--- resources/js/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Alert from "./components/Alert";
-
-const container = document.getElementById("app-react-root");
-
-if (container) {
-    const root = createRoot(container);
-    root.render(<Alert message="This is a Message" type="success" />);
-}
